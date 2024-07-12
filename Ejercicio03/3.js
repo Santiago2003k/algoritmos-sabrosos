abca61cbf0b9bfaf9deb2dbc952812de3231a11c
Santiago Gontier <aguirresanti535@gmial.com>
2024-07-12 14:16:34 -0500

    :)

--- Ejercicio03/3.js
+++ Ejercicio03/3.js
@@ -1,13 +1,17 @@
 function promedio(){
     const resultado = document.getElementById("resultado");
     let inicio = parseInt(document.getElementById("entrada").value);
-    let acum=0;
+    resultado.innerHTML="";
     if (inicio<11 && inicio>0){
         for(let i=1; i<=inicio;i++){
-            resultado.innerHTML="";
-            resultado.innerHTML+="<input type=number id="+i+">";
-            let num=parseInt(document.getElementById("num1"));
-            acum=acum+num;
+            resultado.innerHTML+="<input type=number class='mx-2 my-2' placeholder='Ingrese número'  id= num"+i+">";
+            let num=parseInt(document.getElementById("num "+i));
+        }
+        resultado.innerHTML+="<button class='btn btn-success text-center w-50' type='submit' onclick='sacarP()'>"+"Promediar"+"</button>";
+        function sacarP(){
+            for(let i=1;i<=inicio;i++){
+                
+            }
         }
     }
 }
